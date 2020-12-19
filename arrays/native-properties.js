75024406fa073f09acedc198c5cabc693a8842b2
Kanagu <Kanagu.2210@gmail.com>
2020-12-20 05:30:00 +1100

    Upto Arrays

---
+++ arrays/native-properties.js
@@ -0,0 +1,11 @@
+var box = [];
+
+box[0] = true;
+box[1] = 'meow';
+box['size'] = 9;
+box.area = 100;
+box.push({ 'hello': 'goodbye' });
+
+
+console.log(box.length);
+console.log(box['length']);
--- scripts.js
+++ scripts.js
@@ -1,3 +1,50 @@
 //Use this file to implement Part One of your project
 
 
+var animal = {};
+animal.username = 'Mittens';
+animal['tagline'] = 'Pet me!';
+
+var noises = ['hiss'];
+noises.unshift('purr');
+noises.push('meow')
+noises[3] = 'growl'
+
+console.log(noises.length)
+console.log(noises)
+
+
+animal.noises = noises;
+console.log(animal);
+
+var animals = [];
+animals.push(animal)
+
+var quackers = { username: 'DaffyDuck', tagline: 'Yipeeee!', noises: ['quack 1'] }
+animals[animals.length] = quackers;
+
+var dog = { username: 'Kabosu', tagline: 'high profile!', noises: ['bark', 'wow'] }
+
+var honeybadger = {}
+honeybadger.username = 'Randall'
+honeybadger.tagline = 'Honey badger dont care'
+honeybadger.noises = ['screech', 'growl']
+
+animals.push(dog, honeybadger)
+
+console.log('\n printing animals array \n')
+console.log(animals)
+
+/* var count = 0;
+
+for (var key in animal) {
+    count++;
+    if (key === username) {
+        console.log('Hi my name is' + animal[key]);
+    }
+    else if (key === 'tagline') {
+        console.log('I like to say' + animal[key]);
+    }
+}
+
+ */
